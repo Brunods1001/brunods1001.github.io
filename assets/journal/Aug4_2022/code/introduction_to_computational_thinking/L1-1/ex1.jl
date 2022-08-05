84a4f04b06ceef2ca11be84c2a0ba93a37d9184d
BrunodsLilly <92108779+BrunodsLilly@users.noreply.github.com>
2022-08-05 13:50:33 +0000

    Deploying to gh-pages from  @ 909c8dd1f3a67042931937337a0edf5d3e102be6 🚀

---
+++ assets/journal/Aug4_2022/code/introduction_to_computational_thinking/L1-1/ex1.jl
@@ -0,0 +1,10 @@
+# This file was generated, do not modify it. # hide
+url = "https://user-images.githubusercontent.com/6933510/107239146-dcc3fd00-6a28-11eb-8c7b-41aaf6618935.png"
+
+philip_filename = download(url) # download to a local file. The filename is returned
+philip = load(philip_filename)
+
+a = 10
+
+a * 100
+@show a
