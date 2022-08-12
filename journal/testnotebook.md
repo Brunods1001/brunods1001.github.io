054ee58a9c9e45d9a0dcc82a0b339e08db6c5299
Brunods10 <brunods1001@gmail.com>
2022-08-12 03:59:35 -0400

    Trying to insert notebooks

---
+++ journal/testnotebook.md
@@ -0,0 +1,10 @@
++++
+title = "Testing"
+hascode = true
+date = Date(2022, 8, 12)
+rss = "The goings ons of today"
+
+tags = ["journal", "productivity", "julia", "computational_thinking"]
++++
+
+{{ plutonotebookpage ../notebooks/testingnotebook.jl }}
--- utils.jl
+++ utils.jl
@@ -1,16 +1,69 @@
 function hfun_bar(vname)
-  val = Meta.parse(vname[1])
-  return round(sqrt(val), digits=2)
+    val = Meta.parse(vname[1])
+    return round(sqrt(val), digits=2)
 end
 
 function hfun_m1fill(vname)
-  var = vname[1]
-  return pagevar("index", var)
+    var = vname[1]
+    return pagevar("index", var)
 end
 
 function lx_baz(com, _)
-  # keep this first line
-  brace_content = Franklin.content(com.braces[1]) # input string
-  # do whatever you want here
-  return uppercase(brace_content)
+    # keep this first line
+    brace_content = Franklin.content(com.braces[1]) # input string
+    # do whatever you want here
+    return uppercase(brace_content)
+end
+
+function hfun_plutonotebookpage(params)
+    "Generate Pluto notebook HTML and plug into page"
+    path = params[1]
+    path_to_html = if endswith(path, ".jl")
+        path[1:end-3] * ".html"
+    else
+        path * ".html"
+    end
+    generated_html = """
+    <style>
+    .content {
+        max-width: 100%;
+        margin-right: 0px;
+        padding: 0px;
+        overflow-y: hidden;
+        height: 100vh;
+    }
+    .franklin-content {
+        padding: 0px;
+    }
+    .page-foot {
+        display: none;
+    }
+    .plutopage {
+        height: 100vh;
+    }
+    .smallscreenlink {
+        display: none;
+    }
+    @media (max-width: 768px) {
+        .franklin-content {
+            padding: 0px;
+        }
+    }
+    </style>
+    <h1>Notebook</h1>
+    This is generated HTML.
+    Path to nb: $path_to_html
+
+
+    <iframe width="100%" height="100%"
+    src="$(path_to_html)"
+    class="plutopage"
+    frameborder="0"
+    allow="accelerometer; ambient-light-sensor; autoplay; battery; camera; display-capture; document-domain; encrypted-media; execution-while-not-rendered; execution-while-out-of-viewport; fullscreen; geolocation; gyroscope; layout-animations; legacy-image-formats; magnetometer; microphone; midi; navigation-override; oversized-images; payment; picture-in-picture; publickey-credentials-get; sync-xhr; usb; wake-lock; screen-wake-lock; vr; web-share; xr-spatial-tracking"
+    allowfullscreen></iframe>
+
+
+    <a class="smallscreenlink" href="$(path_to_html)"></a>
+    """
+    return generated_html
 end
