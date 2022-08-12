3cff02964f60b514a4f05585ff0e94bbd92de9a5
Brunods10 <brunods1001@gmail.com>
2022-08-12 10:51:53 -0400

    Trying to insert notebooks

--- journal/testnotebook.md
+++ journal/testnotebook.md
@@ -7,4 +7,21 @@ rss = "The goings ons of today"
 tags = ["journal", "productivity", "julia", "computational_thinking"]
 +++
 
-{{ plutonotebookpage ../notebooks/testingnotebook.jl }}
+# August 12, 2022
+
+\tableofcontents
+
+## What I learned today
+- DevOps: how to configure a Kubernetes cluster and expose its applications through their containers, services, ingresses, and hostnames.
+
+{{ plutonotebookpage ../../notebooks/testingnotebook.jl }}
+## Reflection
+It's been a busy week in my personal life and I have not had much free time. I'm already losing sleep to get work done, but I want to force some time to learn something today. The hard part is choosing something to work on! Whatever I choose to do, I want to see it through to the end!
+
+I think I will choose to follow computational thinking with Julia from MIT, because of three reasons:
+1. I want to learn to model in Julia.
+2. It's written in Julia, so I can get better at the language and also communicate it through this website which is written in a [Julia framework](https://franklinjl.org/workflow/index.html#tag_pages).
+3. It has a structured course load.
+
+**I've had multiple false starts with this course. Here's to the first true start**
+
--- utils.jl
+++ utils.jl
@@ -19,11 +19,14 @@ function hfun_plutonotebookpage(params)
     "Generate Pluto notebook HTML and plug into page"
     path = params[1]
     path_to_html = if endswith(path, ".jl")
-        path[1:end-3] * ".html"
+        joinpath(path[1:end-3], "index.html")
     else
-        path * ".html"
+        joinpath(path, "index.html")
     end
-    generated_html = """
+
+    # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Feature-Policy#directives
+
+    return """
     <style>
     .content {
         max-width: 100%;
@@ -50,20 +53,12 @@ function hfun_plutonotebookpage(params)
         }
     }
     </style>
-    <h1>Notebook</h1>
-    This is generated HTML.
-    Path to nb: $path_to_html
-
-
     <iframe width="100%" height="100%"
     src="$(path_to_html)"
     class="plutopage"
     frameborder="0"
     allow="accelerometer; ambient-light-sensor; autoplay; battery; camera; display-capture; document-domain; encrypted-media; execution-while-not-rendered; execution-while-out-of-viewport; fullscreen; geolocation; gyroscope; layout-animations; legacy-image-formats; magnetometer; microphone; midi; navigation-override; oversized-images; payment; picture-in-picture; publickey-credentials-get; sync-xhr; usb; wake-lock; screen-wake-lock; vr; web-share; xr-spatial-tracking"
     allowfullscreen></iframe>
-
-
     <a class="smallscreenlink" href="$(path_to_html)"></a>
     """
-    return generated_html
 end
